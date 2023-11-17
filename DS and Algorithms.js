b3d630972627f539085d48de98d562533dbaaa73
hazecodez <muhammedhashir298@gmail.com>
2023-11-17 10:37:50 +0530

    first commit

---
+++ DS and Algorithms.js
@@ -0,0 +1,146 @@
+//==================================DATA STRUCTURE==========================================
+//hashSet
+
+// let arr = new Set([7,5,2,3,9,8,0]);
+// let hey = [...arr]
+// let tar =10
+// let arr1 =[]
+// for(let i=0;i<hey.length;i++){
+//   if(arr.has(tar-hey[i])){
+//     arr1.push(hey[i],tar-hey[i])
+//     break;
+//   }
+// }
+// console.log(arr1);
+
+// let arr = [6,1,6,8,10,4,15,6,3,9,6]
+// let len = arr.length;
+// arr = arr.filter(data => data !== 6)
+// let newlen = arr.length;
+// for(let i=1;i<=len-newlen;i++){
+//   arr.push(6)
+// }
+// console.log(arr)
+
+//fibonacci
+// function fibonacci(n){
+//   const fib = [0,1]
+//   for(let i = 2; i < n; i++){
+//     fib[i] = fib[i-1] + fib[i-2]
+//   }
+//   return fib
+// }
+// console.log(fibonacci(1))
+// console.log(fibonacci(3))
+
+//factorial
+// function factorial(n){
+//   if(n===1 || n===0){
+//     return 1
+//   }
+//   return n*factorial(n-1)
+// }
+// console.log(factorial(0))
+
+// function factorial(n){
+//   let res = 1
+//   for(let i =2 ;i<=n; i++){
+//     res = res * i
+//   }
+//   return res
+// }
+// console.log(factorial(3))
+
+function binary(arr,t){
+  let start = 0;
+  let end = arr.length - 1;
+  
+  while(start<=end){
+    let mid = Math.floor((start + end) /2)
+    if(arr[mid]=== t){
+      return mid;
+    }else if(arr[mid]<t){
+      start = mid + 1;
+    }else{
+      end = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binary([1,2,3,4,5,6,7,8,9],8))
+
+function recursive(arr,t){
+  return search(arr,t,0,arr.length-1)
+}
+
+function search(arr,target,start,end){
+  if(start > end){
+    return -1
+  }
+  let mid = Math.floor((start+end)/2)
+  if(arr[mid]==target){
+    return mid
+  }else if(arr[mid]>target){
+    return search(arr,target,start,mid-1)
+  }else{
+    return search(arr,target,mid+1,end)
+  }
+  
+}
+
+// console.log(recursive([1,2,3,4,5,6,7,8,9],9))
+
+class Node {
+  constructor(value){
+      this.value= value;
+      this.next = null;
+      this.prev = null;
+  }
+}
+class Linked {
+  constructor(){
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+  }
+  isEmpty(){
+      return this.size === 0
+  }
+
+  append(value){
+      const node = new Node(value)
+      if(this.isEmpty()){
+          this.head = node;
+          this.tail = node;
+      }else{
+          this.tail.next = node;
+          this.tail = node;
+      }
+      this.size++
+  }
+  print(){
+      if(this.isEmpty()){
+          return null
+      }else{
+          let curr = this.head;
+          let values = ''
+          while(curr){
+              values += `${curr.value}`
+              curr = curr.next
+          }
+          console.log(values)
+      }
+  }
+      
+      
+}
+
+const list = new Linked
+list.append(8)
+list.append(5)
+list.append(2)
+list.append(1)
+list.print()
+
+
---
+++ LinkedList.js
@@ -0,0 +1,175 @@
+//addig node to list
+
+
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList{
+    constructor(){
+      this.head = null;
+      this.size = 0;
+      this.tail = null;
+    }
+    isEmpty(){
+      return this.size === 0;
+    }
+    getSize(){
+      return this.size;
+    }
+  
+  insert(value,index){
+    
+  }
+  
+   removeFrom(index){
+    if(index > this.size || index < 0){
+      return null
+    }
+    let removedNode
+    if(index === 0){
+      removedNode = this.head;
+      this.head = this.head.next;
+    }else{
+      let prev =this.head;
+      for(let i=0;i<index-1;i++){
+        prev = prev.next;
+      }
+      removedNode = prev.next;
+      prev.next = removedNode.next;
+    }
+    this.size--
+    return removedNode.value
+   }
+   removevalue(value){
+    if(this.isEmpty()){
+      return null
+    }
+    if(this.head.value === value){
+      this.head = this.head.next;
+      this.size--;
+    }else{
+      let prev = this.head;
+      while(prev.next && prev.next.value !== value){
+        prev = prev.next;
+      }
+      if(prev.next){
+        let removedNode = prev.next;
+        prev.next = removedNode.next;
+        this.size--
+        return value
+      }
+      return null
+    }
+   }
+   search(value){
+    if(this.isEmpty()){
+      return -1
+    }
+    let curr = this.head;
+    let i = 0;
+    while(curr){
+      if(curr.value === value){
+        return i
+      }
+        curr = curr.next
+        i++
+    }
+    return -1
+   }
+   reverse(){
+    let prev = null;
+    let curr = this.head;
+    while(curr){
+      let next = curr.next;
+      curr.next = prev
+      prev = curr
+      curr = next;
+    }
+    this.head = prev
+   }
+  
+    prepend(value){
+      const node = new Node(value)
+      if(this.isEmpty()){
+        this.head = node;
+        this.tail = node;
+      }else{
+        node.next = this.head;
+        this.head = node;
+      }
+      this.size++
+    }
+    append(value){
+      const node = new Node(value)
+      if(this.isEmpty()){
+        this.head = node;
+        this.tail = node;
+      }else{
+        this.tail.next = node;
+        this.tail = node;
+      }
+      this.size++
+    }
+    removeFront(){
+      if(this.isEmpty()){
+        return null
+      }
+      if(this.size === 1){
+        this.head = null;
+        this.tail = null;
+      }else{
+        this.head = this.head.next
+      }
+      this.size--; 
+    }
+    removeEnd(){
+      if(this.isEmpty()){
+        return null;
+      }
+      if(this.size === 1){
+        this.head =null;
+        this.tail = null;
+      }else{
+        let prev = this.head;
+        while(prev.next !== this.tail){
+          prev = prev.next;
+        }
+        prev.next = null;
+        this.tail = prev;
+      }
+      this.size--
+    }
+  
+    
+    print(){
+      if(this.isEmpty()){
+        console.log('empty')
+      }else{
+        let curr = this.head;
+        let values = '';
+        while(curr){
+          values += `${curr.value} `
+          curr = curr.next;
+        }
+        console.log(values);
+      }
+    }
+  }
+  
+  const list = new LinkedList
+  
+  list.prepend(10)
+  list.prepend(20)
+  list.prepend(30)
+  list.prepend(40)
+  list.append(99)
+  list.print()
+  list.removeFront()
+  list.print()
+  list.removeEnd()
+  list.print()
+  
