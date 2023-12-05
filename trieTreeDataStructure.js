5b28f82b157adb49a46b02f95856aa22b82e58df
hazecodez <muhammedhashir298@gmail.com>
2023-12-05 23:20:12 +0530

    trie array added

--- trieTreeDataStructure.js
+++ trieTreeDataStructure.js
@@ -52,10 +52,135 @@ class trieTree {
             }
         }
     }
+    display(){
+        return this.displayTrie(this.root , "")
+    }
+    displayTrie(node , prefix) {
+        if(node.wordEnd) {
+            console.log(prefix);
+        }
+        for(const [char,child] of node.children) {
+            this.displayTrie(child , prefix + char)
+        }
+    }
+    
 }
 const tries = new trieTree()
-tries.insert('hello')
-tries.insert('hashir')
-// tries.remove('hashir')
+// tries.insert('hello')
+// tries.insert('hashir')
+// // tries.remove('hashir')
 
-console.log(tries.search('hashir'));
+// tries.display()
+
+
+///========================================
+
+class TrieNode {
+    constructor() {
+        this.children = new Array(26);
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let current = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            const index = word.charCodeAt(i) - 'a'.charCodeAt(0);
+            if (!current.children[index]) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEndOfWord = true;
+    }
+
+    search(word) {
+        let current = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            const index = word.charCodeAt(i) - 'a'.charCodeAt(0);
+
+            if (!current.children[index]) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.isEndOfWord;
+    }
+
+    remove(word) {
+        this.removeNode(this.root, word, 0);
+    }
+
+    removeNode(node, word, depth) {
+        if (!node) {
+            return null;
+        }
+
+        if (depth === word.length) {
+            node.isEndOfWord = false;
+        } else {
+            const index = word.charCodeAt(depth) - 'a'.charCodeAt(0);
+            const childNode = node.children[index];
+            this.removeNode(childNode, word, depth + 1);
+
+            if (!node.children[index].isEndOfWord && this.isNodeEmpty(node.children[index])) {
+                node.children[index] = null;
+            }
+        }
+    }
+
+    isNodeEmpty(node) {
+        for (let i = 0; i < node.children.length; i++) {
+            if (node.children[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    display() {
+        this.displayWords(this.root, "");
+    }
+
+    displayWords(node, prefix) {
+        if (node.isEndOfWord) {
+            console.log(prefix);
+        }
+
+        for (let i = 0; i < node.children.length; i++) {
+            if (node.children[i]) {
+                const char = String.fromCharCode('a'.charCodeAt(0) + i);
+                this.displayWords(node.children[i], prefix + char);
+            }
+        }
+    }
+}
+
+// Example usage:
+const trie = new Trie();
+trie.insert('hello');
+// trie.insert('hashir');
+// trie.insert('world');
+
+// console.log("Search 'hashir':", trie.search('hashir')); // true
+// console.log("Search 'hi':", trie.search('hi')); // false
+
+// console.log("Words in the trie:");
+// trie.display();
+
+// console.log("Removing 'hashir'");
+// trie.remove('hashir');
+
+// console.log("Words in the trie after removal:");
+// trie.display();
