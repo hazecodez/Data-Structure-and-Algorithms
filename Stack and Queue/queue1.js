e34e4c2b9f1c0b792cc17ed31629d4237fb41c70
hazecodez <muhammedhashir298@gmail.com>
2024-02-09 03:35:35 +0530

    Queue Structure practise

---
+++ Stack and Queue/queue1.js
@@ -0,0 +1,23 @@
+class Queue {
+    constructor() {
+        this.items = []
+    }
+    enqueue(val) {
+        return this.items.push(val)
+    }
+    dequeue() {
+        return this.items.shift()
+    }
+    print() {
+        return this.items
+    }
+}
+
+const queue1 = new Queue()
+queue1.enqueue(43)
+queue1.enqueue(65)
+queue1.enqueue(76)
+queue1.enqueue(232)
+queue1.enqueue(909)
+queue1.dequeue()
+console.log(queue1.print());
