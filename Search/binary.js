c7e4fe4c1385511fb2eba362a76a9a75e0dcc733
hazecodez <muhammedhashir298@gmail.com>
2024-02-09 03:08:49 +0530

    Binary search practise

--- Search/binary.js
+++ Search/binary.js
@@ -1,18 +1,16 @@
-function binary(arr,t){
-    let start = 0;
-    let end = arr.length - 1;
-    
-    while(start<=end){
-      let mid = Math.floor((start + end) /2)
-      if(arr[mid]=== t){
-        return mid;
-      }else if(arr[mid]<t){
-        start = mid + 1;
-      }else{
-        end = mid - 1;
-      }
+function binary(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
-    return -1;
   }
-  
-  console.log(binary([1,2,3,4,5,6,7,8,9],8))
+  return -1;
+}
+console.log(binary([1, 2, 3, 4, 5, 6, 7, 8, 9], 8));
