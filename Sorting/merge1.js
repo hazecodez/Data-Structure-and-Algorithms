590b4e48b6125900e13c5056feb97db87354f5c7
hazecodez <muhammedhashir298@gmail.com>
2024-02-20 23:18:35 +0530

    mergesort practise

--- Sorting/merge1.js
+++ Sorting/merge1.js
@@ -1,23 +1,44 @@
-function split(array) {
-  if (array.length < 2) {
-    return array;
-  }
-  let mid = Math.floor(array.length / 2);
-  let left = array.slice(0, mid);
-  let right = array.slice(mid);
-  return merge(split(left), split(right));
+// function split(array) {
+//   if (array.length < 2) {
+//     return array;
+//   }
+//   let mid = Math.floor(array.length / 2);
+//   let left = array.slice(0, mid);
+//   let right = array.slice(mid);
+//   return merge(split(left), split(right));
+// }
+// function merge(left, right) {
+//   let sorted = [];
+//   while (left.length && right.length) {
+//     if (left[0] <= right[0]) {
+//       sorted.push(left.shift());
+//     } else {
+//       sorted.push(right.shift());
+//     }
+//   }
+//   return [...sorted, ...left, ...right];
+// }
+
+// let array = [5, -9, -2, 8, 7, 3, 1];
+// console.log(split(array));
+
+
+function mergeSort(arr) {
+  if(arr.length < 2) return arr;
+  let mid = Math.floor(arr.length/2);
+  let left = arr.slice(0,mid);
+  let right = arr.slice(mid);
+  return merging(mergeSort(left),mergeSort(right))
 }
-function merge(left, right) {
+function merging (left,right) {
   let sorted = [];
-  while (left.length && right.length) {
-    if (left[0] <= right[0]) {
-      sorted.push(left.shift());
-    } else {
-      sorted.push(right.shift());
+  while(left.length && right.length) {
+    if(left[0] <= right[0] ) {
+      sorted.push(left.shift())
+    }else {
+      sorted.push(right.shift())
     }
   }
-  return [...sorted, ...left, ...right];
+  return [...sorted, ...left, ...right]
 }
-
-let array = [5, -9, -2, 8, 7, 3, 1];
-console.log(split(array));
+console.log(mergeSort([55,44,33,77,1,3,99,8]));
